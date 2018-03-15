7279a07a2e9a194d2d254f90560de3835ecf4d94
Kaipheu <djambaesaid73@gmail.com>
2018-03-15 11:21:00 +0100

    Modification Bleutooth : Recherche de l'appareil.

--- app/src/main/java/fr/djambae/pilulierautomatique/Bluetooth.java
+++ app/src/main/java/fr/djambae/pilulierautomatique/Bluetooth.java
@@ -1,11 +1,16 @@
 package fr.djambae.pilulierautomatique;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import java.util.Set;
 import java.util.UUID;
 
 
@@ -13,6 +18,19 @@ public class Bluetooth extends AppCompatActivity {
     public static final String TAG = "App";
     private UUID UUID;
     String Nom;
+//    private final BroadcastReceiver Recepteur = new BroadcastReceiver() {
+//        public void onReceive(Context context, Intent intent) {
+//            String action = intent.getAction();
+//            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+//                // Discovery has found a device. Get the BluetoothDevice
+//                // object and its info from the Intent.
+//                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+//                String deviceName = device.getName();
+//                String deviceHardwareAddress = device.getAddress(); // MAC address
+//            }
+//        }
+//    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +39,7 @@ public class Bluetooth extends AppCompatActivity {
         Intent Int = getIntent();
         Nom = Int.getStringExtra("Nom");
         BluetoothAdapter Blue_Adapte = BluetoothAdapter.getDefaultAdapter(); // Objet support Bluetooth
-        //Set<BluetoothDevice> AppareilAssoci = Blue_Adapte.getBondedDevices(); //Objet de stokage de la liste des pareille connecté
+        Set<BluetoothDevice> AppareilAssoci = Blue_Adapte.getBondedDevices(); //Objet de stokage de la liste des pareille connecté
 
         if (Blue_Adapte == null) {// Verification de la présence du bluetooth
 
@@ -32,7 +50,7 @@ public class Bluetooth extends AppCompatActivity {
             toast.show();
 
         } else {
-            if(!Blue_Adapte.isEnabled()){
+            if (!Blue_Adapte.isEnabled()) {
                 CharSequence text = "Activer le Bluetooth !";
                 int duration = Toast.LENGTH_SHORT;
 
@@ -41,9 +59,17 @@ public class Bluetooth extends AppCompatActivity {
 
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableBtIntent, 0);
-
             }
         }
-    }
-}
+        CharSequence text = "Connexion au pilulier";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+        Blue_Adapte.startDiscovery();
+//        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+//        registerReceiver(Recepteur, filter);
 
+
+    }
+
+}
