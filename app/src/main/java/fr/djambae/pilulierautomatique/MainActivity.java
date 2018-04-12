9c03757f82cd33cbc3ceeaee65c0e49814edcb1a
Kaipheu <djambaesaid73@gmail.com>
2018-04-12 23:00:06 +0200

    Finalisation de la connection bluetoooth avec le Hc

--- app/src/main/java/fr/djambae/pilulierautomatique/MainActivity.java
+++ app/src/main/java/fr/djambae/pilulierautomatique/MainActivity.java
@@ -6,32 +6,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+ String Nom ="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent Intent;
+        Intent = getIntent();
+        if(Intent != null){
+            Nom = Intent.getStringExtra("Nom");
+        }
     }
 
     public void sendMessage(View View){
         EditText Texte = (EditText) findViewById(R.id.editText);
         String Message  = Texte.getText().toString();
 
-        if(Message!="Votre Nom" && Message.equals!=""){
+        if(!Message.equals("Votre Nom") && !Message.equals("")){
             Intent intent = new Intent(this, Posologie.class);
             intent.putExtra("Nom", Message);
             startActivity(intent);
+            Nom = Message;
+        }else{
+            CharSequence text = "Entrez votre nom";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(this, text, duration);
+            toast.show();
         }
     }
 
     public void Efface(View View) {
-        if (Message = "Votre Nom" && Message.equals = "") {
-            EditText Texte = (EditText) findViewById(R.id.editText);
+        EditText Texte = (EditText) findViewById(R.id.editText);
+        String Message  = Texte.getText().toString();
+
+        if (Message.equals("Votre Nom") || Message.equals("") || Message.equals(Nom)) {
             Texte.setText("");
         } else {
-            EditText Texte = (EditText) findViewById(R.id.editText);
             sendMessage(Texte);
         }
     }
